90961a893d7666f207b3413396ce6f11c1c2929d
westd72882-glitch <westd72882@gmail.com>
2026-07-25 14:38:55 +0300

    Update MainActivity.kt

--- app/src/main/java/com/example/wasteland/MainActivity.kt
+++ app/src/main/java/com/example/wasteland/MainActivity.kt
@@ -4,14 +4,73 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import java.io.PrintWriter
+import java.io.StringWriter
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            WastelandTheme {
-                GameScreen()
+
+        // Перехватываем любой краш и показываем текст ошибки на экране
+        // вместо мгновенного вылета в чёрный экран. Это временная диагностика.
+        Thread.setDefaultUncaughtExceptionHandler { _, throwable ->
+            val sw = StringWriter()
+            throwable.printStackTrace(PrintWriter(sw))
+            val message = sw.toString()
+            runOnUiThread {
+                setContent {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(Color.Black)
+                            .padding(16.dp)
+                            .verticalScroll(rememberScrollState())
+                    ) {
+                        Text(
+                            text = "КРАШ:\n\n$message",
+                            color = Color.Red,
+                            fontSize = 12.sp
+                        )
+                    }
+                }
+            }
+        }
+
+        try {
+            enableEdgeToEdge()
+            setContent {
+                WastelandTheme {
+                    GameScreen()
+                }
+            }
+        } catch (t: Throwable) {
+            val sw = StringWriter()
+            t.printStackTrace(PrintWriter(sw))
+            setContent {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(Color.Black)
+                        .padding(16.dp)
+                        .verticalScroll(rememberScrollState())
+                ) {
+                    Text(
+                        text = "КРАШ ПРИ ЗАПУСКЕ:\n\n$sw",
+                        color = Color.Red,
+                        fontSize = 12.sp
+                    )
+                }
             }
         }
     }
